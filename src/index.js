8a487ed70a6861b021446ad9188ac6cba7b19db6
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-10-09 14:31:45 +0700

    feat: add unused class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+export class UnusedClass {}
+
 export default class Something {
   log() {
-    console.log('Something')
+    console.log('Something');
   }
   log1(param) {
-    console.log('Something', param)
+    console.log('Something', param);
   }
   log2(param1, param2) {
-    console.log('Something', param1, param2)
+    console.log('Something', param1, param2);
   }
 }
 
 export class Helper {
   constructor(name) {
-    this.name = name
+    this.name = name;
   }
 }
